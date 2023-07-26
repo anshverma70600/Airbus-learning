f07678d0f5b087910c0440fbad1224256eca629f
Ansh verma <anshverma@MacBook-Air-3.local>
2023-07-26 23:05:09 +0530

    commit

---
+++ src/components/CourseList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from "styled-components";
+import Tabs from "./Tabs";
+import { useCoursesContext } from '../context/courses_context';
+
+const CourseList = () => {
+  const {courses} = useCoursesContext();
+
+  return (
+    <CoursesListWrapper>
+      <div className='container'>
+        <div className='courses-list-top'>
+          <h2>Selection of courses</h2>
+          <p>Choose from several online video courses with new additions publihsed </p>
+        </div>
+
+        <Tabs courses = {courses} />
+      </div>
+    </CoursesListWrapper>
+  )
+}
+
+const CoursesListWrapper = styled.div`
+  padding: 40px 0;
+  .courses-list-top p{
+    font-size: 1.8rem;
+  }
+`;
+
+export default CourseList
